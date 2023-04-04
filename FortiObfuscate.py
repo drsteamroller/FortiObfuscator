a7776eadb5b2ebbb61d14b228d85001a9e5af62a
Andrew McConnell <amcconnell886@gmail.com>
2023-04-04 10:16:07 -0400

    new functionality, check README

--- FortiObfuscate.py
+++ FortiObfuscate.py
@@ -11,6 +11,7 @@ contents = []
 og_filename = 0
 vdom_names = dict()
 ip_repl = dict()
+modifiers = []
 
 #REGEX ----> Use "group" function to select the part that matches https://docs.python.org/3/library/re.html#match-objects
 ipaddr4 = r'(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)[.](25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)[.](25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)[.](25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)'
@@ -18,7 +19,7 @@ ipaddr6 = r"(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|
 
 # Helper Functions
 def isRFC1918(ip):
-    a,b,c,d = ip.split('.')
+    a,b,_ = ip.split('.')
 
     # Very explicitly checks if the addresses are RFC 1918 Class A/B/C addresses
     if (int(a) == 10):
@@ -29,7 +30,6 @@ def isRFC1918(ip):
         return(True)
     else:
         return(False)
-
 
 '''
 How it works:
@@ -63,7 +63,6 @@ def isNetMask(ip):
     for val in ip_list:
         if (val != 0):
             for i in range(7, -1, -1):
-                print(val)
                 val = val - pow(2, i)
                 if (val > 0 and not expect_0):
                     continue
@@ -81,15 +80,19 @@ def isNetMask(ip):
             expect_0 = True
     return constant
 
-# Replaces IP addresses with [Letter].[Letter].[Letter].[Letter]
+# Replaces IP addresses
 def replace_ip4(ip):
+    if (isNetMask(ip)):
+        return ip
     if (ip not in ip_repl.keys()):
         repl = ""
-        if (isRFC1918(ip)):
+        if (isRFC1918(ip) and "-privateips" not in modifiers and "-allips" not in modifiers):
             octets = ip.split('.')
             repl = f"{octets[0]}.{octets[1]}.{random.randrange(0, 256)}.{random.randrange(1, 256)}"
+        elif (not isRFC1918(ip) and "-allips" not in modifiers):
+            repl = f"{random.randrange(1, 255)}.{random.randrange(0, 255)}.{random.randrange(0, 255)}.{random.randrange(1, 255)}"
         else:
-            repl = f"{random.randrange(1, 255)}.{random.randrange(0, 256)}.{random.randrange(0, 256)}.{random.randrange(1, 256)}"
+            repl = ip
         ip_repl[ip] = repl
         return repl
     
@@ -98,26 +101,36 @@ def replace_ip4(ip):
         return ip_repl[ip]
 
 def replace_ip6(ip):
-    if (ip not in ip_repl.keys()):
+    if (ip not in ip_repl.keys() and "-allips" not in modifiers):
         repl = f'{hex(random.randrange(1, 65535))[2:]}:{hex(random.randrange(1, 65535))[2:]}:{hex(random.randrange(1, 65535))[2:]}:{hex(random.randrange(1, 65535))[2:]}:{hex(random.randrange(1, 65535))[2:]}:{hex(random.randrange(1, 65535))[2:]}:{hex(random.randrange(1, 65535))[2:]}:{hex(random.randrange(1, 65535))[2:]}'
         ip_repl[ip] = repl
         return repl
-    else:
+    elif ("-allips" not in modifiers):
         return ip_repl[ip]
+    else:
+        return ip
 
 # Program Functions
 def listOptions():
-    print("\nhelp = list this output\nload <file_path> = input file to be obfuscated\nmap = Show mapped addresses, only after 'obf' is run\nexport = export file\nshow = view contents file contents\nobf = begin obfuscation with enabled settings\n")
+    print("\nhelp = list this output\nload <file_path> = input file to be obfuscated\nmap = Show mapped addresses, only after 'obf' is run\nwritemap = Write the output of the map command to a text file\nexport = export file\nshow = view contents file contents\nobf = begin obfuscation with enabled settings\nmods = show modifiers\n")
+
+def listModifiers():
+    print("\nExclude from replacement:\n-privateips\n-allips\n-vpntunnels\n-snmpcommunities\t<-affects both snmpv2c & v3\n")
 
 # Load a configuration file into a list and return the list
 def load(filename):
     c = ""
+    contents.clear()
+    cl_repl = input("\nClear IP map? (Choose 'y' if this is a different site's FortiGate. If the new loaded config is from the same site (therefore, the same private ips), then choose 'n')\n (y, N)> ")
+    if (cl_repl == 'y'):
+        ip_repl.clear()
     try:
         fl = open(filename, 'r')
         c = fl.readlines()
         fl.close()
     except:
-        print("Something when wrong, try full file path\n")
+        print("\nSomething when wrong, try full file path\n")
+    
     return c
 
 # Troubleshooting command to show the contents of what was loaded
@@ -134,26 +147,32 @@ def export():
         new_filename = (b + "_obfuscated." + ext)
     with open(new_filename, 'w') as outfl:
         outfl.writelines(contents)
-    print("Successfully written to: {}".format(new_filename))
+    print("\nSuccessfully written to: {}\n".format(new_filename))
 
-def showMap():
-
+def showMap(op):
     if (not ip_repl):
-        print("You haven't obfuscated a configuration file yet\n")
+        print("\nYou haven't obfuscated a configuration file yet\n")
         return
 
-    ipv4s = "\t===>>> IPv4 ADDRESSES <<<===\n\
-            Original -> Replacement\n"
-    ipv6s = "\t===>>> IPv6 ADDRESSES <<<===\n\
-            Original -> Replacement\n"
+    ipv4s = "\t===>>> IPv4 ADDRESSES <<<===\nOriginal -> Replacement\n"
+    ipv6s = "\t===>>> IPv6 ADDRESSES <<<===\nOriginal -> Replacement\n"
     
     for k, v in ip_repl.items():
         if len(v) > 15:
             ipv6s += f"{k} -> {v}\n"
         else:
             ipv4s += f"{k} -> {v}\n"
-    sep = '=' * 24
-    print(f"{ipv4s}\n{sep}\n{ipv6s}")
+    sep = '=' * 50
+
+    if (op == "p"):
+        print(f"{ipv4s}\n{sep}\n{ipv6s}")
+        return
+    elif (op == "w"):
+        with open(f"{og_filename}_ipmapping.txt", 'w') as vi:
+            vi.write(f"{ipv4s}\n{sep}\n{ipv6s}")
+        print(f"\nMap file written to {og_filename}_ipmapping.txt\n")
+    else:
+        print("\nUnknown option\n")
 
 # Obfuscation main fuction
 def obfuscate():
@@ -162,55 +181,176 @@ def obfuscate():
     if (not contents):
         return("\nYou need to load a file first\n")
 
+    ## FOR LOOP EXT VARS ##
     # Compile the regex found at the top of this program
     is_ip4 = re.compile(ipaddr4)
     is_ip6 = re.compile(ipaddr6, re.MULTILINE)
 
+    # Flags to look for "edit <name>" within snmp/vpn config
+    SNMP = False
+    SNMP_HOSTS = False
+    IPSEC_P1 = False
+    IPSEC_P2 = False
+
+    # Handle naming of snmp and vpn replacement names
+    snmp_comm_num = 1
+    snmp_host_num = 1
+    vpn_p1_num = 1
+    vpn_p2_num = 1
+    vpn_ddns_num = 1
+    vpn_tun_map = {}
+
+    # Debugging
+    x = ""
+
     # Parse through the list containing the lines of the configuration file
-    for line in range(len(contents)):
+    for i, content in enumerate(contents):
+
         # Record the number of leading spaces, so we aren't having awkward lines that aren't in-line
-        leading = " " * re.search('\S', contents[line]).start()
+        leading = " " * re.search('\S', content).start()
         
         # If we see 'set hostname' or 'set alias', replace those with 'US Federal Customer'
-        if ("set hostname" in contents[line] or "set alias" in contents[line]):
-            l = contents[line].strip().split(" ")
+        if ("set hostname" in content or "set alias" in content):
+            l = content.strip().split(" ")
             l[2] = "US FEDERAL CUSTOMER\n"
-            contents[line] = leading + "{} {} {}".format(l[0], l[1], l[2])
+            content = leading + "{} {} {}".format(l[0], l[1], l[2])
         
         # If we see an IP address, check if it's public, and if so, replace it
-        if (is_ip4.search(contents[line])):
-            g = contents[line].strip().split(" ")
-            if (len(g) > 2):
+        if (is_ip4.search(content)):
+            g = content.strip().split(" ")
+            if (len(g) == 3):
                 if ('"' in g[2]):
                     g[2] = g[2][1:-1]
                 g[2] = replace_ip4(g[2])
+            elif (len(g) > 3):
+                for b, ip in enumerate(g[2:]):
+                    g[b + 2] = replace_ip4(ip)
 
-                leading += " ".join(g)
-                contents[line] = leading + "\n"
+            leading += " ".join(g)
+            content = leading + "\n"
 
-        elif (is_ip6.search(contents[line])):
-            print(contents[line])
-            g = contents[line].strip().split(" ")
-            if (len(g) > 2):
+        elif (is_ip6.search(content)):
+            g = content.strip().split(" ")
+            if (len(g) == 3):
                 if ('"' in g[2]):
                     g[2] = g[2][1:-1]
-                g[2] = replace_ip6(g[2].split('/')[0])
+                if ('/' in g[2]):
+                    g[2] = replace_ip6(g[2].split('/')[0]) + g[2].split('/')[1]
+                else:
+                    g[2] = replace_ip6(g[2])
+            elif (len(g) > 3):
+                for b, ip in enumerate(g):
+                    g[b + 2] = replace_ip6(ip)
 
-                leading += " ".join(g)
-                contents[line] = leading + "\n"
-    
+            leading += " ".join(g)
+            content = leading + "\n"
+        
+        ### SNMP Communities ###
+        if ("-snmpcommunities" in modifiers):
+            if ("config system snmp community" in content or "config system snmp user" in content):
+                SNMP = True
+            
+            if (not SNMP_HOSTS and SNMP and "edit" in content):
+                s = content.strip().split(" ")
+                if (len(g) > 1):
+                    s[1] = f'snmp_comm_{snmp_comm_num}'
+                
+                leading += " ".join(s)
+                content = leading + "\n"
+                snmp_comm_num += 1
+
+            if (SNMP and "config hosts" in content):
+                SNMP_HOSTS = True
+
+            if (SNMP_HOSTS and "edit" in content):
+                s = content.strip().split(" ")
+                if (len(g) > 1):
+                    s[1] = f'snmp_host_{snmp_host_num}'
+
+                leading += " ".join(s)
+                content = leading + "\n"
+                snmp_host_num += 1
+
+            if (SNMP and "name" in content):
+                s = content.strip().split(" ")
+                leading += f'{s[0]} {s[1]} FED_SNMP_Community\n'
+                content = leading
+
+            if (SNMP_HOSTS and "end" in content):
+                SNMP_HOSTS = False
+            
+            if (not SNMP_HOSTS and SNMP and "end" in content):
+                SNMP = False
+        
+        ### VPN Tunnel Names ###
+        if ("-vpntunnels" not in modifiers):
+            if ("config vpn ipsec phase1-interface" in content):
+                IPSEC_P1 = True
+
+            if ("config vpn ipsec phase2-interface" in content):
+                IPSEC_P2 = True
+
+            if (IPSEC_P1 and "set remotegw-ddns" in content):
+                v = content.strip().split(" ")
+                
+                repl = f'{vpn_ddns_num}.net'
+                
+                leading += f'{v[0]} {v[1]} {repl}\n'
+                content = leading
+
+            if (IPSEC_P1 and "edit" in content):
+                v = content.strip().split(" ")
+                repl = f'vpn_p1_{vpn_p1_num}'
+                
+                if (v[1] not in vpn_tun_map.keys()):
+                    vpn_tun_map[v[1]] = repl
+                    vpn_p1_num += 1
+                else:
+                    repl = vpn_tun_map[v[1]]
+
+                leading += f"{v[0]} {repl}\n"
+                content = leading
+                
+            if (IPSEC_P2 and "edit" in content):
+                v = content.strip().split(" ")
+                repl = f'vpn_p2_{vpn_p2_num}'
+
+                leading += f"{v[0]} {repl}\n"
+                content = leading
+                vpn_p2_num += 1
+            
+            if (IPSEC_P2 and "set phase1name" in content):
+                v = content.strip().split(" ")
+                try:
+                    repl = vpn_tun_map[v[2]]
+                except KeyError as e:
+                    # Odd case where P2 shows up in the backup before P1
+                    vpn_tun_map[v[2]] = f'vpn_p1_{vpn_p1_num}'
+                    repl = vpn_tun_map[v[2]]
+                
+                leading += f"{v[0]} {v[1]} {repl}\n"
+                content = leading
+
+            if (IPSEC_P1 and "end" in content):
+                IPSEC_P1 = False
+
+            if (IPSEC_P2 and "end" in content):
+                IPSEC_P2 = False
+
+        contents[i] = content
+
     return("\nOperation was successful\n")
 
 print("\n\n::::::::::  ::::::::  :::::::::  ::::::::::                           :::::::::: :::::::::: :::::::::  \n" +
-      ":+:        :+:    :+: :+:    :+: :+:                                  :+:        :+:        :+:    :+: \n" +
-      "+:+        +:+    +:+ +:+    +:+ +:+                                  +:+        +:+        +:+    +:+ \n" +
-      ":#::+::#   +#+    +:+ +#++:++#+  :#::+::#         +#++:++#++:++       :#::+::#   +#++:++#   +#+    +:+ \n" +
-      "+#+        +#+    +#+ +#+    +#+ +#+                                  +#+        +#+        +#+    +#+ \n" +
-      "#+#        #+#    #+# #+#    #+# #+#                                  #+#        #+#        #+#    #+# \n" +
-      "###         ########  #########  ###                                  ###        ########## #########  \n")
+        ":+:        :+:    :+: :+:    :+: :+:                                  :+:        :+:        :+:    :+: \n" +
+        "+:+        +:+    +:+ +:+    +:+ +:+                                  +:+        +:+        +:+    +:+ \n" +
+        ":#::+::#   +#+    +:+ +#++:++#+  :#::+::#         +#++:++#++:++       :#::+::#   +#++:++#   +#+    +:+ \n" +
+        "+#+        +#+    +#+ +#+    +#+ +#+                                  +#+        +#+        +#+    +#+ \n" +
+        "#+#        #+#    #+# #+#    #+# #+#                                  #+#        #+#        #+#    #+# \n" +
+        "###         ########  #########  ###                                  ###        ########## #########  \n")
 
 print("______________________________________________________________________________________________________")
-print("\nFortiObfuscate - Mask hostname, External IP addresses, Usernames, VPN tunnels*, SNMP communities*, etc.\n*Under Construction\n")
+print("\nFortiObfuscate - Mask hostname, IP addresses, Usernames, VPN tunnels, SNMP communities, etc.\n*Under Construction\n")
 
 # user input
 uin = input("--> ")
@@ -225,12 +365,31 @@ while (uin not in 'quit'):
     elif (uin in "show"):
         show()
     elif (uin in "map"):
-        showMap()
+        showMap("p")
     elif (uin in "export"):
         export()
     elif (uin in "obf"):
         print(obfuscate())
-    
+    elif (uin in "mods"):
+        listModifiers()
+    elif (uin in "writemap"):
+        showMap("w")
+    elif ("-" in uin):
+        if ("-allips" == uin):
+            modifiers.append("-allips")
+            print(uin)
+        elif ("-privateips" == uin):
+            modifiers.append("-privateips")
+            print(uin)
+        elif ("-vpntunnels" == uin):
+            modifiers.append("-vpntunnels")
+            print(uin)
+        elif ("-snmpcommunities" == uin):
+            modifiers.append("-snmpcommunities")
+            print(uin)
+        
+        else:
+            print("\nUnrecognized modifier\n")
     # Unrecognized option: list the options
     else:
         listOptions()
--- README.md
+++ README.md
@@ -1,11 +1,42 @@
-FortiObfuscator.py -> anonymizes FortiGate configuration files
+# FortiObfuscator.py -> anonymizes FortiGate configuration files
 
 Takes in a FortiGate configuration file and obfuscates the following:
 
 - hostnames/aliases
-- VPN tunnel names *
+- VPN tunnel names
 - Private & public IP addresses (+ IPv6 addresses)
 - Usernames
-- SNMP tunnels *
+- SNMP communities (v1/2c/3)
 
-\* Under construction
+## Usage
+
+```
+{ py | python } FortiObfuscator.py
+```
+
+This loads the CLI program, the options available:
+
+- Options:
+```
+help = list this output
+load <file_path> = input file to be obfuscated
+map = Show mapped addresses, only after 'obf' is run
+writemap = Write the output of the map command to a text file
+export = export file
+show = view contents file contents
+obf = begin obfuscation with enabled settings
+mods = show modifiers
+```
+
+- Modifiers:
+```
+Exclude from replacement:
+-privateips             <- Excludes JUST private IPs from replacement
+-allips                 <- Excludes public & private IPs from replacement
+-vpntunnels             <- Tunnel Phase 1 & 2 names + DDNS
+-snmpcommunities        <- affects both snmpv2c & v3
+```
+
+## Known Issues
+
+I built this program with the idea that you could load and work with multiple config files. Currently it only works with one at a time.
