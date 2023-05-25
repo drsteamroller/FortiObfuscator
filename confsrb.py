e720c93fc71801e46f7b363fe67337e6cc6a2ae8
Andrew McConnell <amcconnell886@gmail.com>
2023-05-25 10:18:53 -0400

    big rewrite. Acts like other tools

--- confsrb.py
+++ confsrb.py
@@ -5,13 +5,15 @@
 import re
 import random
 import sys
+import os
 
 # Global Variables
 contents = []
-og_filename = 0
+og_filenames = 0
 str_repl = dict()
 ip_repl = dict()
-modifiers = []
+opflags = []
+mod_dir = ""
 
 #REGEX ----> Use "group" function to select the part that matches https://docs.python.org/3/library/re.html#match-objects
 ipaddr4 = r'(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)[.](25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)[.](25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)[.](25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)'
@@ -33,33 +35,33 @@ def isRFC1918(ip):
 
 # a more granular check
 def isValidIP6(addr):
-	if type(addr) == bytes:
-		addr = str(addr)[2:-1]
-	
-	maxcol = 7
-	mincol = 2
-	countcol = 0
-	maxnums = 4
-	countnums = 0
-	validchars = re.compile(r'[A-Fa-f0-9:]')
+    if type(addr) == bytes:
+        addr = str(addr)[2:-1]
+    
+    maxcol = 7
+    mincol = 2
+    countcol = 0
+    maxnums = 4
+    countnums = 0
+    validchars = re.compile(r'[A-Fa-f0-9:]')
 
-	for num in addr:
-		ch = validchars.search(num)
-		if not ch:
-			return False
-		
-		if num in ':':
-			countcol += 1
-			if countnums > maxnums:
-				return False
-			countnums = 0
-		else:
-			countnums += 1
+    for num in addr:
+        ch = validchars.search(num)
+        if not ch:
+            return False
+        
+        if num in ':':
+            countcol += 1
+            if countnums > maxnums:
+                return False
+            countnums = 0
+        else:
+            countnums += 1
 
-	if countcol < mincol or countcol > maxcol:
-		return False
+    if countcol < mincol or countcol > maxcol:
+        return False
 
-	return True
+    return True
 
 '''
 How it works:
@@ -116,10 +118,10 @@ def replace_ip4(ip):
         return ip
     if (ip not in ip_repl.keys()):
         repl = ""
-        if (isRFC1918(ip) and "-privateips" not in modifiers and "-allips" not in modifiers):
+        if (isRFC1918(ip) and "-sPIP" in opflags and "-pi" not in opflags):
             octets = ip.split('.')
             repl = f"{octets[0]}.{octets[1]}.{random.randrange(0, 256)}.{random.randrange(1, 256)}"
-        elif (not isRFC1918(ip) and "-allips" not in modifiers):
+        elif (not isRFC1918(ip) and "-pi" not in opflags):
             repl = f"{random.randrange(1, 255)}.{random.randrange(0, 255)}.{random.randrange(0, 255)}.{random.randrange(1, 255)}"
         else:
             repl = ip
@@ -135,11 +137,11 @@ def replace_ip6(ip):
     if not isValidIP6(ip):
          return ip
 
-    if (ip not in ip_repl.keys() and "-allips" not in modifiers):
+    if ip not in ip_repl.keys() and "-pi" not in opflags:
         repl = f'{hex(random.randrange(1, 65535))[2:]}:{hex(random.randrange(1, 65535))[2:]}:{hex(random.randrange(1, 65535))[2:]}:{hex(random.randrange(1, 65535))[2:]}:{hex(random.randrange(1, 65535))[2:]}:{hex(random.randrange(1, 65535))[2:]}:{hex(random.randrange(1, 65535))[2:]}:{hex(random.randrange(1, 65535))[2:]}'
         ip_repl[ip] = repl
         return repl
-    elif ("-allips" not in modifiers):
+    elif ("-pi" not in opflags):
         return ip_repl[ip]
     else:
         return ip
@@ -161,21 +163,6 @@ def replace_str(s):
     str_repl[s] = repl
 
     return repl
-
-# Program Functions
-def listOptions():
-    print("\nhelp = list this output\n\
-          load <file_path> = input file to be obfuscated\n\
-          map = Show mapped addresses, only after 'obf' is run\n\
-          writemap = Write the output of the map command to a text file\n\
-          importmap <filename> = Import mapfile that was output from FortiObfuscate.py or another FFI program\n\
-          export = export file\n\
-          show = view contents file contents\n\
-          obf = begin obfuscation with enabled settings\n\
-          mods = show modifiers\n")
-
-def listModifiers():
-    print("\nExclude from replacement:\n-privateips\n-allips\n-vpntunnels\n-snmpcommunities\t<-affects both snmpv2c & v3\n\n+clear\t<- Clear modifiers")
 
 # Load a configuration file into a list and return the list
 def load(filename):
@@ -200,14 +187,19 @@ def show():
 # Exports file that was loaded (pre or post obfuscation)
 def export():
     new_filename = ""
-    if (not og_filename):
-        new_filename = input("No filename detected, what do you want the filename to be? > ")
-    else:
-        b, ext = og_filename.split('.')
-        new_filename = (b + "_obfuscated." + ext)
-    with open(new_filename, 'w') as outfl:
-        outfl.writelines(contents)
-    print("\nSuccessfully written to: {}\n".format(new_filename))
+    modfilenames = []
+    for fn in og_filenames:
+        new_filename = fn.replace(".conf", "_mod.conf")
+        if('-d' in args):
+            a = re.search(args[2], fn).span()
+            modfilenames.append(fn[:a[0]] + mod_dir + fn[a[1]:])
+        else:
+            modfilenames.append(new_filename)
+    
+    for index, w_file in enumerate(modfilenames):
+        with open(w_file, 'w') as write:
+            for line in contents[index]:
+                write.write(line)
 
 def showMap(op):
     if (not ip_repl):
@@ -237,77 +229,120 @@ def showMap(op):
             vi.write("+---------- MAPPED STRING VALUES ---------+\n")
             for og, rep in str_repl.items():
                 vi.write(f"Original String: {og}\nMapped String: {rep}\n\n")
-        print(f"\nMap file written to {og_filename}_ipmapping.txt\n")
+        print(f"\nMap file written to {og_filenames}_ipmapping.txt\n")
     else:
         print("\nUnknown option\n")
 
 def importMap(filename):
-	lines = []
-	with open(filename, 'r') as o:
-		lines = o.readlines()
-	
-	print(lines)
+    lines = []
+    with open(filename, 'r') as o:
+        lines = o.readlines()
+    
+    print(lines)
 
-	imp_ip = False
-	imp_mac = False
-	imp_str = False
+    imp_ip = False
+    imp_mac = False
+    imp_str = False
 
-	OG = ""
-	for l in lines:
-		if '+---' in l:
-			if 'IP' in l:
-				imp_ip = True
-				imp_mac = False
-				imp_str = False
-			elif 'MAC' in l:
-				imp_ip = False
-				imp_mac = True
-				imp_str = False
-			elif 'STRING' in l:
-				imp_ip = False
-				imp_mac = False
-				imp_str = True
-			else:
-				print("Map file is improperly formatted, do not make changes to the map file unless you know what you are doing")
-				sys.exit(1)
-			continue
+    OG = ""
+    for l in lines:
+        if '+---' in l:
+            if 'IP' in l:
+                imp_ip = True
+                imp_mac = False
+                imp_str = False
+            elif 'MAC' in l:
+                imp_ip = False
+                imp_mac = True
+                imp_str = False
+            elif 'STRING' in l:
+                imp_ip = False
+                imp_mac = False
+                imp_str = True
+            else:
+                print("Map file is improperly formatted, do not make changes to the map file unless you know what you are doing")
+                sys.exit(1)
+            continue
 
-		if not len(l):
-			continue
+        if not len(l):
+            continue
 
-		if imp_ip:
-			components = l.split(':')
-			if ('Original' in components[0]):
-				OG = components[1].strip()
-			else:
-				ip_repl[OG] = components[1].strip()
-				OG = ""
-		elif imp_mac:
-			components = l.split(':')
-			if ('Original' in components[0]):
-				OG = components[1].strip()
-			else:
-				#mac_repl[OG] = components[1]
-				OG = ""
-		elif imp_str:
-			components = l.split(':')
-			if ('Original' in components[0]):
-				OG = components[1].strip()
-			else:
-				str_repl[OG] = components[1].strip()
-				OG = ""
-		
-		else:
-			print("Something went wrong, mappings might not be fully imported\n")
-			print(f"Interpreted mappings based on import\nIP Mapping: {ip_repl}\nMAC Mapping:\nString Mapping: {str_repl}\n")
+        if imp_ip:
+            components = l.split(':')
+            if ('Original' in components[0]):
+                OG = components[1].strip()
+            else:
+                ip_repl[OG] = components[1].strip()
+                OG = ""
+        elif imp_mac:
+            components = l.split(':')
+            if ('Original' in components[0]):
+                OG = components[1].strip()
+            else:
+                #mac_repl[OG] = components[1]
+                OG = ""
+        elif imp_str:
+            components = l.split(':')
+            if ('Original' in components[0]):
+                OG = components[1].strip()
+            else:
+                str_repl[OG] = components[1].strip()
+                OG = ""
+        
+        else:
+            print("Something went wrong, mappings might not be fully imported\n")
+            print(f"Interpreted mappings based on import\nIP Mapping: {ip_repl}\nMAC Mapping:\nString Mapping: {str_repl}\n")
 
+def buildDirTree(dir):
+    mod_dir = f"{dir}_obfuscated"
+
+    mtd = mod_dir
+
+    dirTree = next(os.walk(dir))[0]
+    slashes = dirTree.count('/') + dirTree.count('\\')
+
+    dirTree = []
+
+    for dirpath, dirnames, fnames in os.walk(dir):
+        check = f"{dirpath}"
+        
+        dirTree.append(check)
+
+    # Create new directory to house the modified files
+    os.makedirs(mod_dir, exist_ok=True)
+
+    moddirTree = dirTree.copy()
+    for i, path in enumerate(moddirTree):
+        a = re.search(dir, path)
+        moddirTree[i] = path[:a.span()[0]] + mod_dir + path[a.span()[1]:]
+
+        os.makedirs(moddirTree[i], exist_ok=True)
+    
+    return (mtd, dirTree)
+
+def getFiles(dirTree):
+    slash = '/'
+
+    files = []
+    # Gotta love Windows
+    if sys.platform == 'win32':
+        slash = '\\'
+    
+    # list comprehension ftw! dir + slash (/ or \) + filename
+    for dir in dirTree:
+        files.extend([f'{dir}{slash}{i}' for i in next(os.walk(dir))[2]])
+        if f'{dir}{slash}{args[0]}' in files:
+            print(f"\nERROR: You cannot perform a confsrb on a directory containing {args[0]} (itself)\n\nexiting...\n")
+            sys.exit()
+    
+    return files
 
 # Obfuscation main fuction
-def obfuscate():
+def obfuscate(conf):
 
     # If no file loaded, prompt to load a file
-    if (not contents):
-        return("\nYou need to load a file first\n")
+    if (not conf):
+        return("\nEmpty\n")
 
     ## FOR LOOP EXT VARS ##
     # Compile the regex found at the top of this program
@@ -326,7 +361,7 @@ def obfuscate():
     x = ""
 
     # Parse through the list containing the lines of the configuration file
-    for i, content in enumerate(contents):
+    for i, content in enumerate(conf):
 
         # Record the number of leading spaces, so we aren't having awkward lines that aren't in-line
         leading = " " * re.search('\S', content).start()
@@ -340,11 +375,14 @@ def obfuscate():
         
         # If we see an IP address, check if it's public, and if so, replace it
         if (is_ip4.search(content)):
+            a = is_ip4.search(content).span()
             g = content.strip().split(" ")
             if (len(g) == 3):
                 if ('"' in g[2]):
                     g[2] = g[2][1:-1]
-                g[2] = replace_ip4(g[2])
+
+                g[2] = content[:a[0]] + replace_ip4(content[a[0]:a[1]]) + content[a[1]:]
+                    
             elif (len(g) > 3):
                 for b, ip in enumerate(g[2:]):
                     g[b + 2] = replace_ip4(ip)
@@ -369,7 +407,7 @@ def obfuscate():
             content = leading + "\n"
         
         ### SNMP Communities ###
-        if ("-snmpcommunities" in modifiers):
+        if ("-ps" in opflags):
             if ("config system snmp community" in content or "config system snmp user" in content):
                 SNMP = True
             
@@ -406,7 +444,7 @@ def obfuscate():
                 SNMP = False
         
         ### VPN Tunnel Names ###
-        if ("-vpntunnels" not in modifiers):
+        if ("-pv" not in opflags):
             if ("config vpn ipsec phase1-interface" in content):
                 IPSEC_P1 = True
 
@@ -448,67 +486,100 @@ def obfuscate():
             if (IPSEC_P2 and "end" in content):
                 IPSEC_P2 = False
 
-        contents[i] = content
+        conf[i] = content
 
-    return("\nOperation was successful\n")
+    return conf
 
-print("\n\n::::::::::  ::::::::  :::::::::  ::::::::::                           :::::::::: :::::::::: :::::::::  \n" +
-        ":+:        :+:    :+: :+:    :+: :+:                                  :+:        :+:        :+:    :+: \n" +
-        "+:+        +:+    +:+ +:+    +:+ +:+                                  +:+        +:+        +:+    +:+ \n" +
-        ":#::+::#   +#+    +:+ +#++:++#+  :#::+::#         +#++:++#++:++       :#::+::#   +#++:++#   +#+    +:+ \n" +
-        "+#+        +#+    +#+ +#+    +#+ +#+                                  +#+        +#+        +#+    +#+ \n" +
-        "#+#        #+#    #+# #+#    #+# #+#                                  #+#        #+#        #+#    #+# \n" +
-        "###         ########  #########  ###                                  ###        ########## #########  \n")
+options = {"-h": "Display this output",\
+           "-g": "Use this option if you are inputting a group of logs. Usage: py logscrub.py -g log1.log,log2.log3.log... <options",\
+           "-d": "Same as -g, but specifying a whole directory. Usage: py logscrub.py -d [path] <options>",\
+           "-sPIP": "Scrub private IPs. Assumes /16 subnet",\
+           "-pi":"preserve all ip addresses",\
+           "-ps":"preserve snmp community names",\
+           "-pv":"preserve vpn phase1/2 names names",\
+           "-map=<mapfilename>":"Import IP/MAC/String mappings from other FFI program output"}
 
-print("______________________________________________________________________________________________________")
-print("\nFortiObfuscate - Mask hostname, IP addresses, Usernames, VPN tunnels, SNMP communities, etc.\n*Under Construction\n")
+args = sys.argv
 
-# user input
-uin = input("--> ")
+if len(args) < 2:
+    print("Usage:\n\tpython confsrb.py <config file> [options] OR\
+                  \n\tpython confsrb.py -g conf1,conf2,... [options] OR\
+                  \n\tpython confsrb.py -d <dir> [options]\n")
+    sys.exit()
 
-# While the user inputted option is not 'q', 'u', 'i', 't' or a combination of those letters
-while (uin not in 'quit'):
+if args[1] == '-g':
+    if len(args) < 3:
+        print("Usage:\n\tpython confsrb.py -g conf1,conf2,... [options]\n")
+        sys.exit()
+    try:
+        og_filenames = [fn for fn in args[2].split(',')]
+        for file in og_filenames:
+            with open(file, 'r') as f:
+                contents.append(f.readlines())        
+    except IndexError:
+        print("Usage:\n\tpython confsrb.py -g conf1,conf2,... [options]\n")
+        sys.exit()
+    except FileNotFoundError:
+        print("Couldn't find one of the files provided")
+        sys.exit()
+    if len(args) > 3:
+        for x in args[3:]:
+            if "-map" in x:
+                mapfn = x.split('=')[1]
+                try:
+                    importMap(mapfn)
+                except:
+                    print(f"Could not read map file: {mapfn}")
+            else:
+                opflags.append(x)
 
-    # Do the option chosen by the user
-    if ("load" in uin):
-        og_filename = uin.split(" ")[1]
-        contents = load(og_filename)
-    elif (uin in "show"):
-        show()
-    elif (uin in "map"):
-        showMap("p")
-    elif (uin in "export"):
-        export()
-    elif (uin in "obf"):
-        print(obfuscate())
-    elif (uin in "mods"):
-        listModifiers()
-    elif (uin in "writemap"):
-        showMap("w")
-        print(str_repl)
-    elif (uin in "importmap"):
-         importMap(uin.split(" ")[1])
-    elif ("-" in uin or '+' in uin):
-        if ("-allips" == uin):
-            modifiers.append("-allips")
-            print(uin)
-        elif ("-privateips" == uin):
-            modifiers.append("-privateips")
-            print(uin)
-        elif ("-vpntunnels" == uin):
-            modifiers.append("-vpntunnels")
-            print(uin)
-        elif ("-snmpcommunities" == uin):
-            modifiers.append("-snmpcommunities")
-            print(uin)
-        elif ("+clear" == uin):
-            modifiers.clear()
-            print("\nCleared modifiers\n")
-        else:
-            print("\nUnrecognized modifier\n")
-    # Unrecognized option: list the options
-    else:
-        listOptions()
 
-    # prompt line
-    uin = input("--> ")
+elif args[1] == '-d':
+    if len(args) < 3:
+        print("\n\tpython confsrb.py -d <dir> [options]\n")
+        sys.exit()
+    if os.path.isfile(args[2]):
+        print(f"Path provided: {args[2]} is a file, not a directory (or does not exist)")
+        sys.exit()
+    
+    print(args[2])
+    dt = buildDirTree(args[2])
+    mod_dir = dt[0]
+    dirtree = dt[1]
+    og_filenames = getFiles(dirtree)
+
+    curfile = ""
+    try:
+        for file in og_filenames:
+            curfile = file
+            with open(file, 'r') as f:
+                contents.append(f.readlines())
+    except:
+        print(f"Could not find file {curfile}")
+        sys.exit()
+
+    if len(args) > 3:
+        for x in args[3:]:
+            if "-map" in x:
+                mapfn = x.split('=')[1]
+                try:
+                    importMap(mapfn)
+                except:
+                    print(f"Could not read map file: {mapfn}")
+            else:
+                opflags.append(x)
+
+else:
+    try:
+        with open(args[1], 'r') as f:
+            contents.append(f.readlines())
+    except:
+        print(f"Could not find file {args[1]}")
+        sys.exit()
+
+obfuscated_contents = []
+
+for conf_file in contents:
+    obfuscated_contents.append(obfuscate(conf_file))
+
+export()
